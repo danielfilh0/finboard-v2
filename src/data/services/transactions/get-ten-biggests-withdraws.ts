9c84a85c1e87d9236b34d649390144e1c022fb5d
Daniel Filho <danbsilva98@gmail.com>
2023-11-26 18:32:08 -0300

    feat: add get biggests transactions service
    Get deposits and withdraws

---
+++ src/data/services/transactions/get-ten-biggests-withdraws.ts
@@ -0,0 +1,15 @@
+import { TransactionWithStatus, TransactionsFilters } from '@/data/types/all'
+import { parseObjToQueryParams } from '@/data/utils/parse-obj-to-query-params'
+
+import { request } from '../http-client'
+
+export async function getTenBiggestsWithdraws(
+  filters: TransactionsFilters,
+): Promise<TransactionWithStatus[]> {
+  const params = parseObjToQueryParams(filters)
+  const data = await request(
+    `/transactions/withdraws?${params}&order-by-amount=desc`,
+  )
+
+  return data.slice(0, 10)
+}
