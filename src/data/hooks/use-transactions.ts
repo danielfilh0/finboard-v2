9d9e11ee198b057fc2ea008dfed492bfd4b1ce63
Daniel Filho <danbsilva98@gmail.com>
2023-11-27 14:54:48 -0300

    refactor: add error handler on useTransactions hook

--- src/data/hooks/use-transactions.ts
+++ src/data/hooks/use-transactions.ts
@@ -1,43 +1,49 @@
+import { redirect } from 'next/navigation'
+
 import { transactionsService } from '@/data/services/transactions'
 
 import { TransactionsFilters } from '../types/all'
 
 export async function useTransactions(filters: TransactionsFilters) {
-  const [
-    transactions,
-    depositsTotal,
-    withdrawsTotal,
-    pendingsTotal,
-    tenBiggestsDeposits,
-  ] = await Promise.all([
-    transactionsService.getAll(filters),
-    transactionsService.getDepositsTotal(filters),
-    transactionsService.getWithdrawsTotal(filters),
-    transactionsService.getPendingsTotal(filters),
-    transactionsService.getTenBiggestsDeposits(filters),
-  ])
+  try {
+    const [
+      transactions,
+      depositsTotal,
+      withdrawsTotal,
+      pendingsTotal,
+      tenBiggestsDeposits,
+    ] = await Promise.all([
+      transactionsService.getAll(filters),
+      transactionsService.getDepositsTotal(filters),
+      transactionsService.getWithdrawsTotal(filters),
+      transactionsService.getPendingsTotal(filters),
+      transactionsService.getTenBiggestsDeposits(filters),
+    ])
 
-  const balance = depositsTotal - withdrawsTotal
+    const balance = depositsTotal - withdrawsTotal
 
-  const barChartLabels = tenBiggestsDeposits.map((deposit) => deposit.account)
-  const barChartData = tenBiggestsDeposits.map((deposit) => deposit.amount)
+    const barChartLabels = tenBiggestsDeposits.map((deposit) => deposit.account)
+    const barChartData = tenBiggestsDeposits.map((deposit) => deposit.amount)
 
-  const lineChartLabels = transactions
-    .map((deposit) => deposit.account)
-    .slice(0, 20)
-  const lineChartData = transactions
-    .map((deposit) => deposit.amount)
-    .slice(0, 20)
+    const lineChartLabels = transactions
+      .map((deposit) => deposit.account)
+      .slice(0, 20)
+    const lineChartData = transactions
+      .map((deposit) => deposit.amount)
+      .slice(0, 20)
 
-  return {
-    depositsTotal,
-    withdrawsTotal,
-    lineChartData,
-    lineChartLabels,
-    barChartLabels,
-    barChartData,
-    balance,
-    pendingsTotal,
-    tenBiggestsDeposits,
+    return {
+      depositsTotal,
+      withdrawsTotal,
+      lineChartData,
+      lineChartLabels,
+      barChartLabels,
+      barChartData,
+      balance,
+      pendingsTotal,
+      tenBiggestsDeposits,
+    }
+  } catch (err: Any) {
+    redirect('/login')
   }
 }
